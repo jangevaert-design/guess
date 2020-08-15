340569fc7e9b113a01b36777b64d808ff3f611f1
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-15 15:46:55 -0600

    updated version

--- src/Guess.java
+++ src/Guess.java
@@ -1,20 +1,27 @@
 public class Guess {
 
   public static void main(String[] args) throws java.io.IOException {
-      char ch, answer = 'K';
+    char ch, answer = 'K';
 
-      System.out.println ("I'm thinking of a letter between A and Z.");
-      System.out.print ("Can you guess it: ");
+    System.out.println("I'm thinking of a letter between A and Z.");
+    System.out.print("Can you guess it: ");
 
-      ch = (char) System.in.read();
+    ch = (char) System.in.read();
 
-      if (ch == answer) {
-        System.out.println("**RIGHT**");
+    if (ch == answer) {
+      System.out.println("**RIGHT**");
 
+    } else {
+      System.out.println("Sorry, you're ");
+
+      if (ch < answer) {
+        System.out.println("too low");
       } else {
-        System.out.println("Sorry, you're wrong.");
+        System.out.println("too high");
       }
 
+
+    }
 
   }
 
